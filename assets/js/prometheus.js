ec3c9f1e85197a50f60a4bca1cf8b46bb0ea7fc8
Tommy Dorsey <tdorsey@gmail.com>
2017-01-28 09:17:18 -0500

    Free Memory displayed via ajax

--- assets/js/prometheus.js
+++ assets/js/prometheus.js
@@ -1,37 +1,63 @@
+document.domain = document.domain;
+prometheus = {
 
-    prometheus = {
+    host: "https://rtd3.me/prometheus",
+    endpoint: "/api/v1/query",
+    free_memory: null,
 
-        host: "https://prometheus.rtd3.me",
-        endpoint: "/api/v1/query",
+    queryFreeMemory: function (oauthToken) {
 
-        getFreeMemory: function (oauthToken) {
-
-            $.ajaxPrefilter(function (options, originalOptions, jqXHR) {
-                options.crossDomain = {
-                    crossDomain: true
-                };
-                options.xhrFields = {
-                    withCredentials: true
-                };
-
-                //jqXHR.setRequestHeader('Authorization',
-                 //   'Bearer ' + oauthToken.access_token);
-            });
-
-            $.ajax(this.host + this.endpoint, {
-                data: {
-                    query: 'node_memory_MemFree / node_memory_MemTotal * 100'
-                }
-            })
-                .then(
-                function success(name) {
-                    alert(name);
+        $.ajax(this.host + this.endpoint,
+            {
+      
+            data: {
+                query: 'node_memory_MemFree / node_memory_MemTotal * 100'
                 },
+        })
+            .then(
+            function success(result) {
+                console.log(JSON.stringify(result));
+                var resultObj = prometheus.free_memory = prometheus.parseResult(result);
 
-                function fail(data, status) {
-                    console.log('Request failed.  Returned status of ' + status);
-                }
-                );
-        },
+                var resultAsPercentage =
+                    Math.round(resultObj.resultVal) + "%";
+                var timestamp = resultObj.timestamp;
+                console.log(timestamp);
 
-    };
+                var utcSeconds = timestamp;
+                var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
+                d.setUTCSeconds(utcSeconds);
+
+                $("#free_memory").append(resultAsPercentage);
+                $("#free_memory_updated").append("Updated: " + d.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+
+            },
+
+            function fail(data, status) {
+                console.log('Request failed.  Returned status of ' + status);
+            }
+            );
+    },
+
+    getFreeMemory: function () {
+        if (!prometheus.free_memory) {
+            prometheus.free_memory = prometheus.queryFreeMemory();
+
+        }
+
+        return prometheus.free_memory;
+
+    },
+
+    parseResult: function (queryResult) {
+
+        var result = {
+            timestamp: queryResult.data.result[0].value[0],
+            resultVal: queryResult.data.result[0].value[1]
+        }
+
+        return result;
+
+    },
+
+};
